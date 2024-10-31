5856a7bcb66ac4e0775e39b23e18838a0477c395
Bianca Madrid <bugamadrid111@gmail.com>
2024-10-31 15:29:12 -0700

    Added Lotprice Attribute and Created the UI for Sales Screen
    I added the Lotprice Attribute including the setter and getter and added it to constructor. I added this to help me determine sales price and monhtly payment depending on price

--- CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
+++ CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
+import java.util.Scanner;
+
+public class SalesContract extends Contract{
+   static Scanner reader = new Scanner(System.in);
+//  TODO
+    //Two Methods for monthly payment (DEPENDENT ON CUSTOMER FINANCES CAR)\
+    //Method for Total Price
+    //Call abstract calls in constructor
+    //use uses if statements for processing?
+    //use if statements for finance car?
 
 public class SalesContract {
 public class SalesContract extends Contract {
@@ -8,13 +18,31 @@ public class SalesContract extends Contract {
     private double ProcessingFee;
     private boolean financeCar;
     private double MonthlyPayment;
+    private static double lotPrice;
+    //Display Screen for Sales
+    //User Input
+    public static void DisplaySales(){
+        System.out.println("Welcome to Sales Screen: \n");
+        System.out.println("To Calculate Total Sales Price of Vehicle please Enter the Following Information: \n");
+        System.out.println("Lot Price: " + lotPrice);
+        reader.nextDouble();
+
+        System.out.println("An Automatic Sales Tax of 5% Included: " + salesTaxAmount );
+        System.out.println("An Automatic Recording Fee is Included " + recordingFee);
+        System.out.println("For Vehicles Under $10,000 the Processing Fee is $295. \n" +
+                "For Vehicles Over $10,000 the Processing Fee is $495.\n" +
+                "Your Processing Fee is : " + processingFee);
+        System.out.println("Will the Vehicle Be Financed? " + financeCar);
+        reader.nextLine();
 
     
     public SalesContract(){
+        System.out.println("Your Monthly Payment will be: " + monthlyPayment);
+        System.out.println("Your Total Price Including all Fees and Add-ons is: " + totalPrice);
 
 
     //Constructor
-    public SalesContract(double salesTaxAmount, double recordingFee, double processingFee,
+    public SalesContract( double lotPrice, double salesTaxAmount, double recordingFee, double processingFee,
                          boolean financeCar){
         //call Abstract class???
         //use if statement for processing fee bc it varies depending on vehicle price
@@ -26,6 +54,11 @@ public class SalesContract extends Contract {
         this.financeCar = financeCar;
 
     //Getters
+
+    public static double getLotPrice() {
+        return lotPrice;
+    }
+
     public double getSalesTaxAmount() {
         return salesTaxAmount;
     }
@@ -43,6 +76,11 @@ public class SalesContract extends Contract {
     }
 
     //Setters
+
+    public static void setLotPrice(double lotPrice) {
+        SalesContract.lotPrice = lotPrice;
+    }
+
     public void setSalesTaxAmount(double salesTaxAmount) {
         this.salesTaxAmount = salesTaxAmount;
     }
