2f7bb65dfc9ad758008a5650ae4929b32917e10a
Bianca Madrid <bugamadrid111@gmail.com>
2024-10-31 12:06:51 -0700

    Atrributes for SalesContract

--- CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
+++ CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
 public class SalesContract {
+public class SalesContract extends Contract {
+    //Attributes for Sales
+    private double SalesTaxAmount = 0.05;
+    private double RecordingFee = 100;
+    private double ProcessingFee;
+    private boolean financeCar;
+    private double MonthlyPayment;
+
+    
+    public SalesContract(){
+
+
+    }
 }
