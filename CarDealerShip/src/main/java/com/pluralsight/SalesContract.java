8b7f185da3befeadc8b4b4f994160caa3f0858b7
Bianca Madrid <bugamadrid111@gmail.com>
2024-10-31 13:39:16 -0700

    Creating if statement for processing fee

--- CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
+++ CarDealerShip/src/main/java/com/pluralsight/SalesContract.java
@@ -18,10 +18,13 @@ public class SalesContract extends Contract {
                          boolean financeCar){
         //call Abstract class???
         //use if statement for processing fee bc it varies depending on vehicle price
+        //do i also use if statement for finance car?
+        //would i also add if statemnt if financed for monhtly payment
         this.salesTaxAmount = salesTaxAmount;
         this.recordingFee = recordingFee;
         this.processingFee = processingFee;
         this.financeCar = financeCar;
+
     //Getters
     public double getSalesTaxAmount() {
         return salesTaxAmount;
