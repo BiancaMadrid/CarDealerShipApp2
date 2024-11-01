987e107570fe2788ae906491b4e62b4171e8b380
Xavier Glenn <xglenn@appdev.yearup.org>
2024-11-01 14:51:18 -0700

    All of my code!

--- Helper.java
+++ Helper.java
@@ -1,9 +1,6 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+package com.pluralsight;
 
 public class Helper {
 
-    public static void showCurrentTime() { LocalTime currentTime;
-        currentTime = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); String formattedTime = currentTime.format(formatter); System.out.println("Current Time: " + formattedTime); }
+    public static void showCurrentTime() { LocalTime currentTime = LocalTime.now(); DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); String formattedTime = currentTime.format(formatter); System.out.println("Current Time: " + formattedTime); }
 }
