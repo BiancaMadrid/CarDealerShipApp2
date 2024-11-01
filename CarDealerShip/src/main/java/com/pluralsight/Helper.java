0d8b8afdb504cef52e35d50ffd913137bfb50a60
Xavier Glenn <xglenn@appdev.yearup.org>
2024-11-01 14:32:51 -0700

    Merge practice.

---
+++ CarDealerShip/src/main/java/com/pluralsight/Helper.java
@@ -0,0 +1,6 @@
+package com.pluralsight;
+
+public class Helper {
+
+    public static void showCurrentTime() { LocalTime currentTime = LocalTime.now(); DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); String formattedTime = currentTime.format(formatter); System.out.println("Current Time: " + formattedTime); }
+}
