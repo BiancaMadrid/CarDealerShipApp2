e2571d1ff74c92a6f7ed274ce53df4a34f898ae1
Xavier Glenn <xglenn@appdev.yearup.org>
2024-11-01 14:40:04 -0700

    Everything should be here now. Only one error which is needing the filename added.

--- Helper.java
+++ Helper.java
@@ -1,4 +1,9 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Helper {
 
-    public static void showCurrentTime() { LocalTime currentTime = LocalTime.now(); DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); String formattedTime = currentTime.format(formatter); System.out.println("Current Time: " + formattedTime); }
+    public static void showCurrentTime() { LocalTime currentTime;
+        currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); String formattedTime = currentTime.format(formatter); System.out.println("Current Time: " + formattedTime); }
 }
